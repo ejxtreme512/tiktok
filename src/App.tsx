4ddf39b57d8e539cf30e046b2a370fa6d7693606
Andrew <andrew.karnis@gmail.com>
2021-05-27 20:36:20 -0400

    add header and flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tokify from './Tokify';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column">
+      <h2>Welcome to Tokify</h2>
+      <div>
+        <Tokify></Tokify>
+      </div>
+
     </div>
   );
 }
