12d17c782ba6c151be0e8dff7a32fd5a7f747c8b
Eric <erickarnis@gmail.com>
2021-06-09 19:10:43 -0400

    move title

--- src/components/Tokify.tsx
+++ src/components/Tokify.tsx
@@ -1,26 +1,14 @@
 import { Divider, TextField } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-import Tok from './Tok';
-import { Tiktok } from "../types/tok";
-import TokBrowser from './TokBrowser';
+import ToksByTrending from './ToksByTrending';
 
 function Tokify() {
-    const [trendCount, setTrendCount] = useState<number>(30);
-    const [toks, setToks] = useState<Tiktok[]>([]);
-
-    useEffect(() => {
-        let params = new URLSearchParams();
-        params.append(`count`, `${trendCount}`);
-        fetch('http://127.0.0.1:5000/trending', {method: 'GET'}).then(resp => resp.json().then(res => setToks(res)));
-    }, []);
-
     return (
         <div className='flex flex-column tokify pad-10 overflow-auto'>
-            <TokBrowser toks={toks} title="Trending"></TokBrowser>
+            <h2>Welcome to Tokify</h2>
+            <ToksByTrending></ToksByTrending>
         </div>
     );
 }
-
-
 
 export default Tokify;
