d288d22b7281c8ab9bb3bf3e7064aada242915fe
Eric <erickarnis@gmail.com>
2021-06-08 22:43:45 -0400

    add component to browser a given list of tiktoks

---
+++ src/components/TokBrowser.tsx
@@ -0,0 +1,44 @@
+import { Divider, TextField } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import Tok from './Tok';
+import { Tiktok } from "../types/tok";
+
+function TokBrowser(props: {toks: Tiktok[], title: string}) {
+    const [filteredToks, setFilteredToks] = useState<Tiktok[]>(props.toks);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event?.target.value);
+    }
+    const searchToks = (term: string, toks: Tiktok[]) => {
+        if (!term) {
+            return toks;
+        }
+        return toks.filter(tok => tok.desc.toLowerCase().indexOf(term.toLowerCase()) !== -1);
+    }
+
+    useEffect(() => {
+        setFilteredToks(searchToks(searchTerm, props.toks));
+    }, [searchTerm]);
+    useEffect(() => {
+        setFilteredToks(props.toks);
+    }, [props.toks]);
+    return (
+        <div className='flex flex-column tok-browser overflow-auto'>
+            <h3>{props.title}</h3>
+            <div>
+                <TextField id="txtSearch" label="Search" value={searchTerm} onChange={handleSearch} variant="outlined" />
+            </div>
+            <div className='flex-column overflow-auto margin-left-auto margin-right-auto'>
+                {filteredToks.map((tok, index) => (
+                    <div key={index}>
+                        <Tok tiktok={tok}></Tok>
+                        <Divider />
+                    </div>
+                ))}
+                <Divider />
+            </div>
+        </div>
+    );
+}
+
+export default TokBrowser;
