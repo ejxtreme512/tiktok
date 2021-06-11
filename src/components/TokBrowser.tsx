142eb9af01d18075255dae23cc65b577814e8d3e
Eric <erickarnis@gmail.com>
2021-06-10 23:45:42 -0400

    2 toks per row

--- src/components/TokBrowser.tsx
+++ src/components/TokBrowser.tsx
@@ -6,6 +6,7 @@ import "./TokBrowser.css";
 
 function TokBrowser(props: { toks: Tiktok[], title: string }) {
     const [filteredToks, setFilteredToks] = useState<Tiktok[]>(props.toks);
+    const [toksPerRow, setToksPerRow] = useState<number>(2);
     const [searchTerm, setSearchTerm] = useState<string>('');
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(event?.target.value);
@@ -16,35 +17,49 @@ function TokBrowser(props: { toks: Tiktok[], title: string }) {
         }
         return toks.filter(tok => tok.desc.toLowerCase().indexOf(term.toLowerCase()) !== -1);
     }
-    const onMoreInfoSelected = (id: string) => {
-        console.log(id);
-    }
     useEffect(() => {
         setFilteredToks(searchToks(searchTerm, props.toks));
     }, [searchTerm]);
     useEffect(() => {
         setFilteredToks(props.toks);
     }, [props.toks]);
+
     const noToksMatch = (<div>
         <h4>No Toks Match</h4>
     </div>);
-    const tokDisplay = filteredToks && filteredToks.length ? filteredToks.map((tok, index) => (
-        <div key={index}>
-            <Tok tiktok={tok} onMoreInfoSelected={onMoreInfoSelected}></Tok>
-        </div>
-    )) : noToksMatch;
+    let tokDisplay;
+
+
+    tokDisplay = filteredToks && filteredToks.length > 0 ? getTokBreakdown(toksPerRow, filteredToks) : noToksMatch;
+
     return (
         <div className='flex flex-column tok-browser overflow-auto'>
             <div className="flex flex-1 overflow-auto">
                 <div className="sidebar">
                     <TextField id="txtSearch" label="Filter By Description" value={searchTerm} onChange={handleSearch} variant="outlined" />
                 </div>
-                <div className="flex-column flex-1 overflow-auto tok-display">
+                <div className="flex-column flex-1 overflow-auto">
                     {tokDisplay}
                 </div>
             </div>
         </div>
     );
+}
+
+const getTokBreakdown = (toksPerRow: number, filteredToks: Tiktok[]) => {
+    const onMoreInfoSelected = (id: string) => {
+        console.log(id);
+    }
+    const tokRows = [];
+    for (let x = 0; x < filteredToks.length; x += toksPerRow) {
+        const toks = [];
+        for (let y = 0; y < toksPerRow; y++) {
+            const location = x + y;
+            toks.push(<Tok key={location} tiktok={filteredToks[location]} onMoreInfoSelected={onMoreInfoSelected}></Tok>)
+        }
+        tokRows.push(<div key={x} className="flex flex-1">{toks}</div>);
+    }
+    return tokRows;
 }
 
 export default TokBrowser;
