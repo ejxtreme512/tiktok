1520ff7d13924ab1a79d2319731bff382bafa6b9
Eric <erickarnis@gmail.com>
2021-06-14 21:02:49 -0400

    update route to users

--- src/components/TokTabs.tsx
+++ src/components/TokTabs.tsx
@@ -7,7 +7,7 @@ import ToksByUser from "./ToksByUser";
 import { Divider } from '@material-ui/core';
 
 function TokTabs() {
-    const allTabs = ['/trending', '/authors'];
+    const allTabs = ['/trending', '/users'];
 
     return (
         <BrowserRouter>
@@ -19,7 +19,7 @@ function TokTabs() {
                             <div className="flex">
                                 <Tabs orientation="vertical" variant="scrollable"value={location.pathname}>
                                     <Tab label="Trending" value="/trending" component={Link} to={allTabs[0]} />
-                                    <Tab label="Authors" value="/authors" component={Link} to={allTabs[1]} />
+                                    <Tab label="Users" value="/users" component={Link} to={allTabs[1]} />
                                 </Tabs>
                             </div>
                             <Divider orientation="vertical"></Divider>
