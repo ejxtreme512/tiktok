7519a55958e1c2d8621ee0f028ce0fa045242d7b
Eric <erickarnis@gmail.com>
2021-06-17 19:32:54 -0400

    update routes

--- src/constants/routes.tsx
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-    DOWNLOAD_BY_ID: (id: number) => `/download/${id}`,
-    TIKTOKS_BY_TRENDING: () => '/tiktoks/trending',
-    TIKTOKS_BY_USER: (user: string) => `tiktoks/${user}`,
-
-}
--- src/utils/url.tsx
+++
@@ -1,5 +0,0 @@
-export function createURL(address: string, params: any[]) {
-    const urlObj = new URL('address');
-    const urlParams = new URLSearchParams(address).toString();
-    return urlObj
-}
