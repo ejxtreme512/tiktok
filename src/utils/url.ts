722377893f8c2721173be640e0ed09d8f5fdfc01
Eric <erickarnis@gmail.com>
2021-06-17 20:47:09 -0400

    update url creator

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,15 +1,16 @@
 import { ROUTES } from "../constants/routes";
 
 export function createURL(route: any, routeArgs: any[] = [], paramArgs?: any) {
-    const pieces = []
+    const pieces = [];
     const base = '127.0.0.1:500/';
     const routeAddress = ROUTES[route](...routeArgs);
-    const params = new URLSearchParams("a=apple&b=balloon");
+    const params = new URLSearchParams("");
+    pieces.push(base, routeAddress);
     for(let key in paramArgs) {
         params.append(key, paramArgs[key]);
     }
     if (Object.keys(paramArgs).length > 0) {
-        pieces.push('/' + params.toString());
+        pieces.push('?' + params.toString());
     }
-    return new URL(pieces.join());
+    return pieces.join('');
 }
