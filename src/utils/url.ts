bcfd8106dd17e593b6b1ab3e16daee2182bbc0f7
Eric <erickarnis@gmail.com>
2021-06-17 20:16:01 -0400

    update url fn

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,12 +1,15 @@
-import { RouteName, ROUTES } from "../constants/routes";
+import { ROUTES } from "../constants/routes";
 
-
-export function createURL(route: RouteName, routeArgs: any = [], params?: any[]) {
-    const env = '127.0.0.1:500/';
-    const routeAddress = ROUTES[route](...(routeArgs || []));
-    // const urlObj = new URL(address);
-    // if (params) {
-    //     const urlParams = new URLSearchParams(address).toString();
-    // }
-    // return urlObj.toString();
+export function createURL(route: any, routeArgs: any[] = [], paramArgs?: any) {
+    const pieces = []
+    const base = '127.0.0.1:500/';
+    const routeAddress = ROUTES[route](...routeArgs);
+    const params = new URLSearchParams("a=apple&b=balloon");
+    for(let key in paramArgs) {
+        params.append(key, paramArgs[key]);
+    }
+    if (Object.keys(paramArgs).length > 0) {
+        pieces.push('/' + params.toString());
+    }
+    return new URL(pieces.join());
 }
