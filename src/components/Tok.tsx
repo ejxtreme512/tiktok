e35d11fe260abc4d5fbc0011abcfd1da269cd715
Eric <erickarnis@gmail.com>
2021-06-20 21:59:49 -0400

    update lint issues

--- src/components/Tok.tsx
+++ src/components/Tok.tsx
@@ -76,8 +76,8 @@ function Tok(props: { onMoreInfoSelected: (author: User) => void, tiktok: Tiktok
 const intToString = (value: number) => {
     const suffixes = ["", "k", "m", "b","t"];
     const suffixNum = Math.floor((""+value).length/3);
-    let shortValue: string | number = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
-    if (shortValue % 1 != 0) {
+    let shortValue: string | number = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+    if (shortValue % 1 !== 0) {
         shortValue = shortValue.toFixed(1);
     }
     return shortValue+suffixes[suffixNum];
