be6a0c089b51ba7ad93afcdc0c4fc96690ee7fa9
Eric <erickarnis@gmail.com>
2021-06-20 21:01:36 -0400

    update tabs value by pathname

--- src/components/TokTabs.tsx
+++ src/components/TokTabs.tsx
@@ -1,14 +1,16 @@
 import React, { Fragment } from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 import ToksByTrending from "./ToksByTrending";
 import ToksByUser from "./ToksByUser";
 import { Divider } from '@material-ui/core';
 
 function TokTabs() {
-    const allTabs = ['/trending', '/users'];
-
+    const allTabs = ['/trending', '/users', '/favorites'];
+    const getLocation = (location: any) => {
+        return '/' + location.pathname.split("/")[1];
+    }
     return (
         <BrowserRouter>
             <div className="flex flex-1 overflow-auto">
@@ -17,7 +19,7 @@ function TokTabs() {
                     render={({ location }) => (
                         <Fragment>
                             <div className="flex">
-                                <Tabs orientation="vertical" variant="scrollable"value={location.pathname}>
+                                <Tabs orientation="vertical" variant="scrollable" value={getLocation(location)}>
                                     <Tab label="Trending" value="/trending" component={Link} to={allTabs[0]} />
                                     <Tab label="Users" value="/users" component={Link} to={allTabs[1]} />
                                 </Tabs>
