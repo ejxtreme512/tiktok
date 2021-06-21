9a3a5d7f7828eeccbee86208e7a876b018392f13
Eric <erickarnis@gmail.com>
2021-06-20 21:18:11 -0400

    add section for creator info

--- src/components/ToksByUser.tsx
+++ src/components/ToksByUser.tsx
@@ -8,14 +8,14 @@ import { createURL } from '../utils/url';
 import TokBrowser from './TokBrowser';
 
 function ToksByUser(props: { author?: User }) {
-    const location = useLocation();
     const params: any = useParams();
     const [loading, setLoading] = useState<boolean>(false);
+    const [author, setAuthor] = useState<User | null>(props.author || null);
     const getUserName = (): string => {
         if (params.userNameParam) {
             return params.userNameParam;
         }
-        return props.author?.uniqueId || ''
+        return author?.uniqueId || ''
     }
     const [derivedUserName, setUserName] = useState<string>(getUserName());
     const [trendCount, setTrendCount] = useState<number>(30);
@@ -23,39 +23,45 @@ function ToksByUser(props: { author?: User }) {
     useEffect(() => {
         if (derivedUserName) {
             setLoading(true);
-            fetch(createURL(RouteName.TIKTOKS_BY_USER, [derivedUserName]), { method: 'GET' })
-            .then(resp => resp.json()
-            .then(res => {
-                setLoading(false);
-                setToks(res)
-            }));
+            fetch(createURL(RouteName.TIKTOKS_BY_USER, [derivedUserName], { count: trendCount }), { method: 'GET' })
+                .then(resp => resp.json()
+                    .then(res => {
+                        setLoading(false);
+                        setToks(res)
+                    }));
         }
-    }, [props.author]);
+    }, [author]);
 
     useEffect(() => {
-        if (!props.author) {
-            console.log('author info not found, fetch it!');
+        if (!author) {
+            fetch(createURL(RouteName.USER_BY_USERNAME, [derivedUserName]), { method: 'GET' })
+                .then(resp => resp.json())
+                .then(res => {
+                    setAuthor(res.userInfo.user);
+                });
         }
     }, [derivedUserName]);
     const loadingBar = <div className="flex flex-1 flex-align-center pad-5">
         <CircularProgress />
     </div>;
     const emptyDiv = <div></div>;
+    const searchField =  (<TextField id="txtSearch"
+    label="Search Creators"
+    value={derivedUserName} onChange={(evt) => { setUserName(evt.target.value) }}
+    variant="outlined"
+    InputProps={{
+        startAdornment: (
+            <InputAdornment position="start">
+                <Search />
+            </InputAdornment>
+        )
+    }} />);
     const userToks = <div className='flex flex-column toks-by-user pad-5 overflow-auto'>
-        <div className='flex'>
-            <TextField id="txtSearch"
-                label="Search Creators"
-                value={derivedUserName} onChange={(evt) => { setUserName(evt.target.value) }}
-                variant="outlined"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <Search />
-                        </InputAdornment>
-                    )
-                }} />
+        {searchField}
+        <div className="flex">
+            <p>Creator Info here</p>
         </div>
-        {toks ? <TokBrowser toks={toks} title="Creator Videos"></TokBrowser> : emptyDiv};
+        {toks && toks.length ? <TokBrowser toks={toks} title="Creator Videos"></TokBrowser> : emptyDiv};
     </div>;
     return (
         loading ? loadingBar :
