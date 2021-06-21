1a2a6b914b0f646dbac070c38e82e30924742165
Eric <erickarnis@gmail.com>
2021-06-20 20:52:08 -0400

    add route for user info

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,8 @@
 export enum RouteName {
     DOWNLOAD_BY_ID = "DOWNLOAD_BY_ID",
     TIKTOKS_BY_TRENDING = "TIKTOKS_BY_TRENDING",
-    TIKTOKS_BY_USER = "TIKTOKS_BY_USER"
+    TIKTOKS_BY_USER = "TIKTOKS_BY_USER",
+    USER_BY_USERNAME = "USER_BY_USERNAME"
 };
 
 interface Routes {
@@ -12,4 +13,5 @@ export const ROUTES: Routes = {
     [RouteName.DOWNLOAD_BY_ID]: (id: number): string  => `download/${id}`,
     [RouteName.TIKTOKS_BY_TRENDING]: (): string => 'tiktoks/trending',
     [RouteName.TIKTOKS_BY_USER]: (user: string): string => `tiktoks/${user}`,
+    [RouteName.USER_BY_USERNAME]: (user:string): string => `users/${user}`
 };
