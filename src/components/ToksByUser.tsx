6778bcff0f2744fcbf3d1adeccf7490befffab77
Eric <erickarnis@gmail.com>
2021-06-22 19:18:07 -0400

    replace author with user

--- src/components/ToksByUser.tsx
+++ src/components/ToksByUser.tsx
@@ -1,72 +1,65 @@
 import { CircularProgress, InputAdornment, TextField } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 import { RouteName } from '../constants/routes';
 import { User, Tiktok } from "../types/tok.interface";
 import { createURL } from '../utils/url';
 import TokBrowser from './TokBrowser';
 
-function ToksByUser(props: { author?: User }) {
+function ToksByUser(props: { user?: User }) {
     const params: any = useParams();
+    const location = useLocation();
+    const appHistory = useHistory();
+
     const [loading, setLoading] = useState<boolean>(false);
-    const [author, setAuthor] = useState<User | null>(props.author || null);
     const getUserName = (): string => {
-        if (params.userNameParam) {
-            return params.userNameParam;
-        }
-        return author?.uniqueId || ''
+        return params.userNameParam || '';
     }
-    const [derivedUserName, setUserName] = useState<string>(getUserName());
+    const [searchUserName, setUserName] = useState<string>(getUserName());
     const [trendCount, setTrendCount] = useState<number>(30);
     const [toks, setToks] = useState<Tiktok[]>([]);
     useEffect(() => {
-        if (derivedUserName) {
+        if (props.user) {
             setLoading(true);
-            fetch(createURL(RouteName.TIKTOKS_BY_USER, [derivedUserName], { count: trendCount }), { method: 'GET' })
+            fetch(createURL(RouteName.TIKTOKS_BY_USER, [props.user.uniqueId], { count: trendCount }), { method: 'GET' })
                 .then(resp => resp.json()
                     .then(res => {
                         setLoading(false);
                         setToks(res)
                     }));
+            appHistory.push({
+                pathname: `/users/${props.user.uniqueId}`,
+                state: {
+                    update: true,
+                },
+            });
         }
-    }, [author]);
+    }, [props.user]);
 
-    useEffect(() => {
-        if (!author) {
-            fetch(createURL(RouteName.USER_BY_USERNAME, [derivedUserName]), { method: 'GET' })
-                .then(resp => resp.json())
-                .then(res => {
-                    setAuthor(res.userInfo.user);
-                });
-        }
-    }, [derivedUserName]);
     const loadingBar = <div className="flex flex-1 flex-align-center pad-5">
         <CircularProgress />
     </div>;
     const emptyDiv = <div></div>;
-    const searchField =  (<TextField id="txtSearch"
-    label="Search Creators"
-    value={derivedUserName} onChange={(evt) => { setUserName(evt.target.value) }}
-    variant="outlined"
-    InputProps={{
-        startAdornment: (
-            <InputAdornment position="start">
-                <Search />
-            </InputAdornment>
-        )
-    }} />);
-    const userToks = <div className='flex flex-column toks-by-user pad-5 overflow-auto'>
+    const searchField = (<TextField id="txtSearch"
+        label="Search Creators"
+        value={searchUserName} onChange={(evt) => { setUserName(evt.target.value) }}
+        variant="outlined"
+        InputProps={{
+            startAdornment: (
+                <InputAdornment position="start">
+                    <Search />
+                </InputAdornment>
+            )
+        }} />);
+    const tokBrowser = <TokBrowser toks={toks} title="Creator Videos"></TokBrowser>;
+    return <div className='flex flex-column toks-by-user pad-5 overflow-auto'>
         {searchField}
         <div className="flex">
             <p>Creator Info here</p>
         </div>
-        {toks && toks.length ? <TokBrowser toks={toks} title="Creator Videos"></TokBrowser> : emptyDiv};
-    </div>;
-    return (
-        loading ? loadingBar :
-            userToks
-    );
+        {loading ? loadingBar : tokBrowser};
+</div>;
 }
 
 export default ToksByUser;
