c8f7bfc10aa5fd3f986f176ae3552007ed45a2c9
Eric <erickarnis@gmail.com>
2021-06-22 19:20:11 -0400

    manage selected user on tabs

--- src/components/TokTabs.tsx
+++ src/components/TokTabs.tsx
@@ -1,15 +1,32 @@
-import React, { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 import ToksByTrending from "./ToksByTrending";
 import ToksByUser from "./ToksByUser";
 import { Divider } from '@material-ui/core';
+import { User } from "../types/tok.interface";
+import { createURL } from "../utils/url";
+import { RouteName } from "../constants/routes";
 
 function TokTabs() {
     const allTabs = ['/trending', '/users', '/favorites'];
+    const [selectedUser, setSelectedUser] = useState<User>();
     const getLocation = (location: any) => {
         return '/' + location.pathname.split("/")[1];
+    }
+    const onUserSelected = (user: User) => {
+        setSelectedUser(user);
+    }
+
+    const fetchUserInfo = (searchUserName: string) => {
+        if (!selectedUser || selectedUser && selectedUser.uniqueId !== searchUserName) {
+            fetch(createURL(RouteName.USER_BY_USERNAME, [searchUserName]), { method: 'GET' })
+                .then(resp => resp.json())
+                .then(res => {
+                    setSelectedUser(res.userInfo.user);
+                });
+        }
     }
     return (
         <BrowserRouter>
@@ -26,8 +43,8 @@ function TokTabs() {
                             </div>
                             <Divider orientation="vertical"></Divider>
                             <Switch>
-                                <Route path={allTabs[0]} render={(props) => <ToksByTrending></ToksByTrending>} />
-                                <Route path={[`${allTabs[1]}/:userNameParam`, allTabs[1]]} render={(props: any) => <ToksByUser {...props}></ToksByUser>} />
+                                <Route path={allTabs[0]} render={(props) => <ToksByTrending onUserSelected={onUserSelected}></ToksByTrending>} />
+                                <Route path={[`${allTabs[1]}/:userNameParam`, allTabs[1]]} render={(props: any) => <ToksByUser {...props} onUserSelected={onUserSelected}></ToksByUser>} />
                             </Switch>
                         </Fragment>
                     )}
