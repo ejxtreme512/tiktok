3725fd13f76e54d35190064c04c1295a8d2a38a1
Eric <erickarnis@gmail.com>
2021-06-22 21:31:54 -0400

    create number utility

---
+++ src/utils/number.ts
@@ -0,0 +1,9 @@
+export const intToString = (value: number) => {
+    const suffixes = ["", "k", "m", "b","t"];
+    const suffixNum = Math.floor((""+value).length/3);
+    let shortValue: string | number = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+    if (shortValue % 1 !== 0) {
+        shortValue = shortValue.toFixed(1);
+    }
+    return shortValue+suffixes[suffixNum];
+}
