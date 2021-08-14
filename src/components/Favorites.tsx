e0ce12f2589cbfec3f9688bbcc0a39de3e0adf01
Eric <erickarnis@gmail.com>
2021-08-14 13:56:42 -0400

    handle loading

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,19 +1,23 @@
-import { List, ListItem, ListItemIcon, ListItemText, TextField } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+import { CircularProgress, List, ListItem, ListItemIcon, ListItemText, TextField } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import Tok from './Tok';
 import { User, Tiktok, AuthorInfo } from "../types/tok.interface";
 import "./TokBrowser.css";
 import { createURL } from '../utils/url';
 import { RouteName } from '../constants/routes';
+import TokBrowser from './TokBrowser';
 
-function FavoriteToks(props: { favorites: any[]}) {
+function FavoriteToks(props: { favorites: any[] }) {
     const [selectedList, setSelectedList] = useState<any>();
-    const [toks, setToks] = useState<any[]>();
+    const [toks, setToks] = useState<Tiktok[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
     const getList = (listId: number) => {
+        setLoading(true);
         fetch(createURL(RouteName.FAVORITE_LIST, [listId]), { method: 'GET' })
             .then(resp => resp.json()
                 .then(res => {
                     setToks(res);
+                    setLoading(false);
                 }));
     }
     const handleListItemClick = (event: any, fav: any) => {
@@ -30,7 +34,7 @@ function FavoriteToks(props: { favorites: any[]}) {
     const favoriteLists =
         (<List component="nav" aria-label="">
             {props.favorites && props.favorites.map((fav, idx) =>
-                <ListItem button selected={selectedList && selectedList[0] === fav[0]} onClick={(event) => handleListItemClick(event, fav)}>
+                <ListItem key={idx} button selected={selectedList && selectedList[0] === fav[0]} onClick={(event) => handleListItemClick(event, fav)}>
                     <ListItemIcon>
                         {/* <Inbox /> */}
                     </ListItemIcon>
@@ -38,9 +42,14 @@ function FavoriteToks(props: { favorites: any[]}) {
                 </ListItem>
             )}
         </List>)
+    const loadingBar = <div className="flex flex-1 flex-align-center pad-5">
+        <CircularProgress />
+    </div>;
+    const tokBrowser = loading ? loadingBar : (<TokBrowser toks={toks} title=""></TokBrowser>);
     return (
         <div className='flex flex-column tok-favorites overflow-auto'>
             {props.favorites ? favoriteLists : noFavorites}
+            {tokBrowser}
         </div>
     );
 }
